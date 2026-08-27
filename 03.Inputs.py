e4d8a227b312769ae5e912ad61ef7d1bbc3e4fa3
ThiagoCoser <thiago.coser@gmail.com>
2026-08-27 07:13:59 -0300

    Update

---
+++ 03.Inputs.py
@@ -0,0 +1,10 @@
+# Este algoritmo trabalha com variáveis simples e inputs
+
+# String
+seuNome = input("Digite seu nome: ")
+
+# Int
+suaIdade = int(input("Digite sua idade: "))
+
+# Resultado
+print ("Seu nome é", seuNome, "e sua idade é", suaIdade, "anos!")
